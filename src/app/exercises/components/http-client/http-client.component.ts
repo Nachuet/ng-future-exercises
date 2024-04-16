ab3d92d77b7df4721cb8338372fbb38c3cd3dfed
Nachuet <148787557+Nachuet@users.noreply.github.com>
2024-04-16 08:47:07 +0000

    Sincronizar

--- src/app/exercises/components/http-client/http-client.component.ts
+++ src/app/exercises/components/http-client/http-client.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-http-client',
   standalone: true,
-  template: ``,
+  templateUrl: './http-client.component.html',
 })
 export class HttpClientComponent {}
