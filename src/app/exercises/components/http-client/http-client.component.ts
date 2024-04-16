79c3a3d7490de973bebb1e14c2cc67b7ef09dd35
Nachuet <148787557+Nachuet@users.noreply.github.com>
2024-04-16 09:43:26 +0000

    Httpclient

--- src/app/exercises/components/http-client/http-client.component.ts
+++ src/app/exercises/components/http-client/http-client.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core'
 
+/*
+interface Post  {
+  userId : number,
+  id : number, 
+  title : string,
+  body : string
+}*/
+
 @Component({
   selector: 'app-http-client',
   standalone: true,
   templateUrl: './http-client.component.html',
 })
-export class HttpClientComponent {}
+export class HttpClientComponent {
+  getPosts() {}
+  createPost() {}
+  getError() {}
+}
