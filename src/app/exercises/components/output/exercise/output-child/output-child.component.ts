714ac633ddf0c12af5d1bade205679a2b2fb3fc2
Nachuet <nachoif@gmail.com>
2024-04-18 01:06:37 +0200

    Output

--- src/app/exercises/components/output/exercise/output-child/output-child.component.ts
+++ src/app/exercises/components/output/exercise/output-child/output-child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, output } from '@angular/core'
 
 @Component({
   selector: 'app-output-child',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core'
   standalone: true,
   imports: [],
 })
-export class OutputChildComponent {}
+export class OutputChildComponent {
+  onNameChange = output<number>()
+  numero = 0
+
+  increase() {
+    this.numero++
+    this.onNameChange.emit(this.numero)
+  }
+  decrease() {
+    this.numero--
+    this.onNameChange.emit(this.numero)
+  }
+}
--- src/app/exercises/components/output/exercise/output-parent.component.ts
+++ src/app/exercises/components/output/exercise/output-parent.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core'
+import { OutputChildComponent } from './output-child/output-child.component'
 
 @Component({
   selector: 'app-output-parent',
   templateUrl: './output-parent.component.html',
   styleUrl: './output-parent.component.css',
   standalone: true,
-  imports: [],
+  imports: [OutputChildComponent],
 })
-export class OutputParentComponent {}
+export class OutputParentComponent {
+  value: number | undefined
+  handleNameChange(event: number) {
+    this.value = event
+  }
+}
